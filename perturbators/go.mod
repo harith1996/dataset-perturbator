d0313062b6c75ae1f1f60d3d35f6e51cf822b6ef
Harith Rathish <harith1996@gmail.com>
2023-09-07 16:16:26 +0200

    added ApplyPert entry point

--- perturbators/go.mod
+++ perturbators/go.mod
@@ -1,3 +1,7 @@
 module perturbators
 
 go 1.21.0
+
+replace structs => ../structs
+
+require structs v0.0.0-00010101000000-000000000000
--- perturbators/perturbators.go
+++ perturbators/perturbators.go
@@ -1,10 +1,18 @@
 package perturbators
 
-import "fmt"
+import (
+	"fmt"
+	"structs"
+)
+
+func ApplyPert(request structs.PertRequest) string {
+	message := fmt.Sprintf("Applying %v to dataset", request.Perturb)
+	return message
+}
 
 // AddNoise returns a greeting for the named person.
 func AddNoise(name string) string {
-    // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome! I am the noise adder :)" , name)
-    return message
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome! I am the noise adder :)", name)
+	return message
 }
