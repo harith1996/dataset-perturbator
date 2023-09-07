5dfea44c7ee7b0db3330b0e51a2064445e95efda
Harith Rathish <harith1996@gmail.com>
2023-09-07 14:26:57 +0200

    Getting started: Added basic REST api

--- api.go
+++ api.go
@@ -1,10 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "perturbators"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
+
+type PertRequest struct {
+	RawData      string `json:"rawData"`
+	Perturb      string `json:"perturb"`      // "add_noise", "downsample", "jitter"
+	PerturbLevel int    `json:"perturbLevel"` // 1-3
+}
+
+var pertRequests = []PertRequest{
+	{RawData: "x,y\n1,2\n2,3", Perturb: "add_noise", PerturbLevel: 1},
+	{RawData: "x,y\n14,27\n23,38", Perturb: "jitter", PerturbLevel: 2},
+	{RawData: "x,y\n19,22\n25,32", Perturb: "downsample", PerturbLevel: 3},
+}
+
+// getPertRequests responds with the list of all albums as JSON.
+func getPertRequests(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, pertRequests)
+}
+
 func main() {
-    message := perturbators.AddNoise("Martin")
-    fmt.Println(message)
+	router := gin.Default()
+	router.GET("/pertRequests", getPertRequests)
+	router.Run("localhost:8080")
 }
