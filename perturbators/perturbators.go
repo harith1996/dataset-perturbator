0c5a02e4e35f3b10022c9207d031ab47bdcde764
Harith Rathish <harith1996@gmail.com>
2023-09-08 10:46:11 +0200

    csv parsing now working

--- perturbators/perturbators.go
+++ perturbators/perturbators.go
@@ -2,11 +2,20 @@ package perturbators
 
 import (
 	"fmt"
+	"strings"
 	"structs"
+	"github.com/go-gota/gota/dataframe"
 )
+
+func ParseRawData(rawData string) dataframe.DataFrame {
+	df := dataframe.ReadCSV(strings.NewReader(rawData))
+	fmt.Println(df)
+	return df
+}
 
 func ApplyPert(request structs.PertRequest) string {
 	message := fmt.Sprintf("Applying %v to dataset", request.Perturb)
+	ParseRawData(request.RawData)
 	return message
 }
 
@@ -18,13 +27,13 @@ func AddNoise(name string) string {
 }
 
 func Jitter(name string) string {
-    // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome! I am the jitterer :)", name)
-    return message
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome! I am the jitterer :)", name)
+	return message
 }
 
 func Downsample(name string) string {
-    // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome! I am the downsampler :)", name)
-    return message
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome! I am the downsampler :)", name)
+	return message
 }
