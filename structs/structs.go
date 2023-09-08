7c5eb9e223529d94c09b48e09a2f79625e96f9d9
Harith Rathish <harith1996@gmail.com>
2023-09-08 10:42:58 +0200

    added ID field, renamed to "addNoise"

--- structs/structs.go
+++ structs/structs.go
@@ -1,7 +1,8 @@
 package structs
 
 type PertRequest struct {
+	ID           string `json:"id"` // UUID
 	RawData      string `json:"rawData"`
-	Perturb      string `json:"perturb"`      // "add_noise", "downsample", "jitter"
+	Perturb      string `json:"perturb"`      // "addNoise", "downsample", "jitter"
 	PerturbLevel int    `json:"perturbLevel"` // 1-3
 }
